6a2fa8f2298a294700bd500466dff99b2fcd6ccc
Yashi Bagoli <31111010+yashi32@users.noreply.github.com>
2023-10-08 18:45:22 +0530

    Update 206. Reverse_Linked_List.java

--- 206. Reverse_Linked_List.java
+++ 206. Reverse_Linked_List.java
@@ -1,1 +1,19 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
 
+        ListNode curr=head;
+        ListNode next=null;
+        ListNode prev=null;
+
+        while(curr ! = null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        return prev;
+        
+    }
+}
